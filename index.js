698905cec493093ef5550d23dd7d19b958542bfb
wexdanya <wexdaniil@gmail.com>
2023-02-18 17:26:37 +0200

    Add css

--- index.js
+++ index.js
@@ -6,22 +6,34 @@ fetch("./data/data.json")
   })
   .then((data) => {
     if (data) {
-        const title = document.createElement("h1");
-        title.setAttribute('class','title')
-        title.innerText= 'Actors';
-        wrapper.append(title);
-        const actorsList = document.createElement('div');
-        actorsList.setAttribute('class','actorsList')
-        wrapper.append(actorsList)
-        data.forEach((element) => {
-          if (element) {
-            const actorImage = document.createElement('img');
-            actorImage.setAttribute('class','actorImage')
-            actorImage.src = `${element.profilePicture}`
-            console.log(element);
-            actorsList.append(actorImage);
-          }
-        });
-    }
+      const title = document.createElement("h1");
+      title.setAttribute("class", "title");
+      title.innerText = "Actors";
 
+      const actorsList = document.createElement("div");
+      actorsList.setAttribute("class", "actorsList");
+
+      wrapper.append(title)
+      wrapper.append(actorsList)
+
+      data.forEach(({ profilePicture ,firstName, lastName }) => {
+        const img = new Image();
+        img.src = `${profilePicture}`;
+        img.addEventListener(
+          "load",
+          function () {
+            const actorCard = document.createElement("div")
+            actorsList.append(actorCard);
+            actorCard.setAttribute('class',"actorCard")
+            const actorName = document.createElement('p')
+            actorName.setAttribute('class',"actorName")
+            actorName.append(firstName +" "+lastName)
+            img.setAttribute("class", "actorImage");
+            img.setAttribute("alt", `123`);
+            actorCard.append(img);
+            actorCard.append(actorName)
+          }
+        );
+      });
+    }
   });
